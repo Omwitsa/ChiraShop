7ed3a4c901a976e69e7b32affc5f72273db8e4dc
Omwitsa <omwitsawilson@yahoo.com>
2026-03-08 22:25:08 +0300

    Save changes

--- app/Models/ProductCategory.php
+++ app/Models/ProductCategory.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class ProductCategory extends Model
 {
-    protected $table = 'courses';
+    protected $table = 'product_categories';
 
     protected $fillable = [
         'name',
