23018e2c4dfe30291920547c340c913ff59a4250
Omwitsa <omwitsawilson@yahoo.com>
2026-03-14 20:49:06 +0300

    Save changes

--- app/Livewire/Client/Shop.php
+++ app/Livewire/Client/Shop.php
@@ -4,19 +4,40 @@ namespace App\Livewire\Client;
 
 use Livewire\Component;
 use App\Models\Product;
+use Illuminate\Support\Facades\DB;
 
 class Shop extends Component
 {
-    public $products;
+    public $productCategories;
     public function mount()
     {
-        $this->products = Product::all();
+        $this->productCategories = DB::table('product_categories')
+        ->join('products', 'product_categories.name', '=', 'products.category')
+        ->select('product_categories.name', 'product_categories.sortOrder')
+        ->orderBy('product_categories.sortOrder')
+        ->distinct()
+        ->get();
+        
+        $this->categoryProducts();
+    }
+
+    public function categoryProducts()
+    {
+        foreach ($this->productCategories as $key => $value) {
+            $productCategory = (object) $value;
+            $products= DB::select('SELECT * FROM products WHERE category = ? AND active = true', [$productCategory->name]);
+            // foreach ($products as $p_key => $p_value) {
+            //     $product = (object) $p_value;
+            //     $product->focused = false;
+            // }
+            $productCategory->products = $products;
+        }
     }
 
     public function render()
     {
         return view('livewire.client.shop')->with([
-            'products' => $this->products,
+            'productCategories' => $this->productCategories,
         ]);
     }
 }
--- resources/views/livewire/client/shop.blade.php
+++ resources/views/livewire/client/shop.blade.php
@@ -24,29 +24,37 @@
                     </div>
 
                     <div class="card-body">
-                        <div class="row">
-                            @foreach ($products as $index => $product)
-                                <div class="col-xs-12 col-sm-3">
-                                    <div class="row type">
-                                        <div class="col-xs-12">
-                                            <img src="{{ asset('storage'.env('IMG_STORAGE').$product->picUrl) }}" alt="Beauty" style="width:100%;">
-                                        </div>
-                                    </div><br>
+                        @foreach ($productCategories as $index => $category)
+                            <div class="row">
+                                <div class="col-xs-12">
+                                    <h3> {{$category->name}} </h3> 
 
                                     <div class="row">
-                                        <div class="col-sm-12 text-center">
-                                        <p> {{$product->name}} </p> 
-                                            @if (auth()->guard('client')->check())
-                                                <div class="form-group">
-                                                    <a href="" style="color: #FFFFFF" class="btn waves-effect waves-light btn-primary btn-outline-primary" wire:navigate>
-                                                    <i class="ti-shopping-cart"></i>Add to Cart</a>
+                                        @foreach ($category->products as $index1 => $product)
+                                            <div class="col-xs-12 col-sm-3">
+                                                <div class="row type">
+                                                    <div class="col-xs-12">
+                                                        <img src="{{ asset('storage'.env('IMG_STORAGE').$product->picUrl) }}" alt="Beauty" style="width:100%;">
+                                                    </div>
+                                                </div><br>
+
+                                                <div class="row">
+                                                    <div class="col-sm-12 text-center">
+                                                    <p> {{$product->name}} </p> 
+                                                        @if (auth()->guard('client')->check())
+                                                            <div class="form-group">
+                                                                <a href="" style="color: #FFFFFF" class="btn waves-effect waves-light btn-primary btn-outline-primary" wire:navigate>
+                                                                <i class="ti-shopping-cart"></i>Add to Cart</a>
+                                                            </div>
+                                                        @endif
+                                                    </div>
                                                 </div>
-                                            @endif
-                                        </div>
+                                            </div>
+                                        @endforeach
                                     </div>
                                 </div>
-                            @endforeach
-                        </div>
+                            </div>
+                        @endforeach
                     </div>
                 </div>
             </div>
